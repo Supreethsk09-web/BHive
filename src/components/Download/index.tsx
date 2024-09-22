189975f83780e6a71900e345ad16575539656848
supreethsk <supreeth1997@gmail.com>
2024-09-22 18:20:59 +0530

    overview responsive

--- src/components/Download/index.tsx
+++ src/components/Download/index.tsx
@@ -1,12 +1,23 @@
 import { Container } from 'react-bootstrap'
 import DownloadImage from '../../assets/download.png'
+import Download2 from '../../assets/downloadCrop2.png'
+import arrow from '../../assets/Arrow 3.png';
 
 const download = () => {
 return(
     <div>
     <Container>
+        <div className="row m-0 downloadmobile">
+            <div className="col-11 p-0 headerText downloadmobile">
+            Download our app now
+            </div>
+            <div className="col-1 headerText downloadmobile m-xs-auto">
+            <img src={arrow} />
+            </div>
+        </div>
     <div className='row m-0'>
-        <img src={DownloadImage} alt="download" />
+        <img  className='downloadimage' src={DownloadImage} alt="download" />
+        <img className='downloadmobile' src={Download2} alt="down"/>
     </div>
     </Container>
 </div>
