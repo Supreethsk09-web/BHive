ca75d84467fdfcca6e181b7ee703624e3bbfe4d4
supreethsk <supreeth1997@gmail.com>
2024-09-22 17:17:13 +0530

    overview responsive

--- src/components/Choose/index.tsx
+++ src/components/Choose/index.tsx
@@ -3,7 +3,7 @@ import Gym from '../../assets/amen.png';
 import Wifi from '../../assets/wifi.png';
 import Coffee from '../../assets/coffee.png';
 import Cost from '../../assets/cost.png';
-import Chair from '../../assets/amen.png';
+import Chair from '../../assets/chair.png';
 import Time from '../../assets/time.png';
 import Play from '../../assets/play.png';
 import '../Choose/choose.css'
@@ -17,7 +17,7 @@ return (
         </div>
     </Container>
         <div className="mt-4">
-            <div className="container">
+            <div className="container consider">
                 <div  className="box-center chooseText">
                     <img src={Gym} alt="img" />
                 <span className="px-2">Community Events</span>
@@ -63,6 +63,54 @@ return (
                 <span className="px-2">
                 Sports Area
                 </span>
+                </div>
+            </div>
+            <div className="container considerDiv item">
+                <div  className="chooseText chooseContainer">
+                    <img src={Gym} alt="img" />
+                <div className="px-2 mt-2">Community Events</div>
+                </div>
+                <div  className="chooseText chooseContainer">
+                <img src={Gym} alt="img" />
+                <div className="px-2 mt-2">
+                Gym Facilities
+                </div>
+                </div>
+                <div  className="chooseText chooseContainer">
+                <img src={Wifi} alt="img" />
+                <div className="px-2 mt-2">
+                High-Speed WiFi
+                </div>
+                </div>
+                <div  className="chooseText chooseContainer">
+                <img src={Coffee} alt="img" />
+                <div className="px-2 mt-2">
+                Cafe & Tea Bar
+                </div>
+                </div>
+                <div  className="chooseText chooseContainer">
+                <img src={Cost} alt="img" />
+                <div className="px-2 mt-2">
+                Affordable
+                </div>
+                </div>
+                <div  className="chooseText chooseContainer">
+                <img src={Chair} alt="img" />
+                <div className="px-2 mt-2">
+                Comfort Lounges
+                </div>
+                </div>
+                <div  className="chooseText chooseContainer">
+                <img src={Time} alt="img" />
+                    <div className="px-2 mt-2">
+                    Quick Booking
+                    </div>
+                </div>
+                <div  className="chooseText chooseContainer">
+                <img src={Play} alt="img" />
+                <div className="px-2 mt-2">
+                Sports Area
+                </div>
                 </div>
             </div>
         </div>
--- src/components/Overview/Card/index.tsx
+++ src/components/Overview/Card/index.tsx
@@ -34,7 +34,7 @@ const Cards = (props: { info: any[]; }) => {
     <Container fluid>
       <Row>
         {props.info.map((card, idx) => (
-          <Col md={4} key={idx} className="mb-4">
+          <Col md={6} xs={12} xl={4} lg={4} key={idx} className="mb-4">
             <div className='cardContainer'>
                 <div className='row'>
             <div className="col-9">
@@ -78,8 +78,8 @@ const Cards = (props: { info: any[]; }) => {
 
                     </div>
                     <div className='col buttonPrimary'>
-                        <div className="row">
-                            <div className="col-10">
+                        <div className="row m-0">
+                            <div className="col-10 p-0">
                             <div className='buttonText'>
                        Bulk Pass
                     </div>
@@ -87,7 +87,7 @@ const Cards = (props: { info: any[]; }) => {
                      ₹ {card.day_pass_price}<span>/ Days</span>
                     </div>
                             </div>
-                            <div className="col-2 p-0 d-flex align-items-center">
+                            <div className="col-2 p-0 d-flex align-items-center justify-content-end">
                             <svg width="21" height="12" viewBox="0 0 21 12" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path opacity="0.6" fill-rule="evenodd" clip-rule="evenodd" d="M13.7071 5.29289C14.0976 5.68342 14.0976 6.31658 13.7071 6.70711L8.70711 11.7071C8.31658 12.0976 7.68342 12.0976 7.29289 11.7071C6.90237 11.3166 6.90237 10.6834 7.29289 10.2929L11.5858 6L7.29289 1.70711C6.90237 1.31658 6.90237 0.683417 7.29289 0.292893C7.68342 -0.0976314 8.31658 -0.0976313 8.70711 0.292893L13.7071 5.29289Z" fill="#263238"/>
 <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd" d="M6.70711 5.29289C7.09763 5.68342 7.09763 6.31658 6.70711 6.70711L1.70711 11.7071C1.31658 12.0976 0.683417 12.0976 0.292893 11.7071C-0.0976315 11.3166 -0.0976314 10.6834 0.292893 10.2929L4.58579 6L0.292893 1.70711C-0.097631 1.31658 -0.097631 0.683417 0.292893 0.292893C0.683417 -0.0976314 1.31658 -0.0976313 1.70711 0.292893L6.70711 5.29289Z" fill="#263238"/>
